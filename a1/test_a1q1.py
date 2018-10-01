4cb00d8ba32180935816b2bfbf729b57e5f49dba
Willis Kirkham <wrk027@mail.usask.ca>
2018-10-01 11:25:15 -0600

    Creating file and adding function prototypes

---
+++ a1/test_a1q1.py
@@ -0,0 +1,15 @@
+# Willis Kirkham
+# 11084378
+# wrk027
+# C317 - a1
+
+# Synopsis: a class for describing the problem state
+
+from a1.ProblemState import ProblemState
+
+def main():
+    test_problem = ProblemState(5)
+    test_problem.display()
+
+if __name__ == "__main__":
+    main()
